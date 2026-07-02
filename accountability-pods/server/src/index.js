f25804bdef914d0709410470d8c418a30c1900a6
Akshar Rawat <aksharrawat7@gmail.com>
2026-07-02 20:43:38 +0530

    feat: initialize server with Express, MongoDB connection, and CORS support
    - Added package.json for server configuration and dependencies
    - Created app.js to set up Express application with CORS middleware
    - Introduced constants.js for database name configuration
    - Implemented database connection logic in db/index.js using Mongoose
    - Set up index.js to configure environment variables and start the server

---
+++ accountability-pods/server/src/index.js
@@ -0,0 +1,24 @@
+import  dotenv from "dotenv";
+import { app } from "./app.js";
+import connectDb from "./db/index.js";
+
+
+dotenv.config({
+    path:"./.env",
+    quiet:true,
+})
+
+
+connectDb().then(() => {
+  app.on("error", (error) => {
+    console.log("error in connect db ", error);
+    throw error;
+  });
+
+  app.listen(process.env.PORT || 5000, () => {
+    console.log(`server is running at port ${process.env.PORT}`);
+  });
+}).catch((error)=>{
+    console.log("mongo db connection failed !!!",error);
+    
+})
